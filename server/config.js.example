d35cf77d73ddeeb503f2e0da1b3691ea85b7cad6
Kerberos9 <mll.kerberos@gmail.com>
2019-03-28 14:36:19 +0100

    Changed deployment and updated dependencies

---
+++ server/config.js.example
@@ -0,0 +1,3 @@
+module.exports = {
+    frontend_origin: 'http://localhost:3000',
+};
--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const { db, root, frontend_origin } = require('./config');
+const { frontend_origin } = require('./config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
